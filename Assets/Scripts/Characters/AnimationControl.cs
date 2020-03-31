d95d1de77c57431defc03b62b087ce4ad3ce6b75
Stialt <34300269+Stialt@users.noreply.github.com>
2020-03-31 14:23:49 +0900

    [0.6.0] Character Control
     ### Changes
     - Refactored code: separate movement control and animation control
     - Better Character Control: moving following Camera (turning with A,D is removed)
     - Refactor code for character control: introduce states system for animations
     - Reworked code to use CharacterController
     - Improve Character Control: add Jump
     - Imported jump animations from Mixamo
     - Added icon
    
     ### Fixes
     - Fixed minor bugs with disabling buttons while UI is active

---
+++ Assets/Scripts/Characters/AnimationControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+
+    public GameObject thePlayer;
+    private Animator animator;
+
+    public const int IDLE = 0, IDLE_CARRYING = 1, RUNNING = 2,
+        RUNNING_FAST = 3, RUNNING_CARRYING = 4, JUMPING_INPLACE = 5, JUMPING_RUNNING = 6;
+
+    public static int PLAYER_STATE = 0;
+    public static int DIRECTION_STATE = 0;
+
+    public static int PREVIOUS_STATE = 0;
+
+    void Start()
+    {
+        animator = thePlayer.GetComponent<Animator>();    
+    }
+
+    void Update()
+    {
+        string animName;
+
+        if (PLAYER_STATE != PREVIOUS_STATE)
+        {
+            PREVIOUS_STATE = PLAYER_STATE;
+            switch (PLAYER_STATE)
+            {
+                case IDLE:
+                    animName = getRandomIdleAnim(1, 5);
+                    break;
+                case IDLE_CARRYING:
+                    animName = "CarryingIdle";
+                    break;
+                case RUNNING:
+                    animName = "RunNinja";
+                    break;
+                case RUNNING_FAST:
+                    animName = "Fast Run";
+                    break;
+                case RUNNING_CARRYING:
+                    animName = "RunWIthObject";
+                    break;
+                case JUMPING_RUNNING:
+                    animName = "JumpNinja";
+                    //animName = "JumpWide";
+                    break;
+                case JUMPING_INPLACE:
+                    animName = "JumpInPlace";
+                    break;
+                default:
+                    animName = getRandomIdleAnim(1, 5);
+                    break;
+            }
+            animator.Play(animName);
+        }
+    }
+
+
+    string getRandomIdleAnim(int min, int max)
+    {
+        int val = Random.Range(min, max);
+
+        string str;
+
+        switch (val)
+        {
+            case 0: str = "Victory Idle"; break;
+            case 1: str = "Kneeling Idle"; break;
+            case 2: str = "Goalkeeper Idle"; break;
+            case 3: str = "Ninja Idle"; break;
+            case 4: str = "Happy Idle"; break;
+            case 5: str = "Sleeping Idle"; break;
+            default: str = "Ninja Idle"; break;
+        }
+
+        return str;
+    }
+}
--- Assets/Scripts/Characters/CharControl.cs
+++ Assets/Scripts/Characters/CharControl.cs
@@ -7,15 +7,13 @@ public class CharControl : MonoBehaviour
 {
 
     public GameObject thePlayer;
-    public float horizontalMove, verticalMove;
     public int stepNum;
 
     //Flags
-    public bool isStepping = false;
+    public bool isStepping = false;     //true if step sound is currently playing
     public bool isRunning;
     private bool fastRunning = false;
-
-    public static bool changeToNormalAnimation = false;
+    private bool isJumping = false;
 
     public AudioSource footStep1;
     public AudioSource footStep2;
@@ -27,98 +25,163 @@ public class CharControl : MonoBehaviour
     private Stopwatch stopwatch = new Stopwatch();
     private long normalIdleTimeStamp = 0;
 
+    public float speed = 40.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float camRotateSpeed = 180;
+    private float ZMove = 0, XMove = 0, YMove = 0;
+    private float distToGround;
+
+    private CharacterController characterController;
+
+
 
     private void Start()
     {
-        stopwatch.Start();
+        //[0.6.0] Sleeping State is temporarily removed 
+        //stopwatch.Start();
+
         GetComponent<Rigidbody>().inertiaTensorRotation = Quaternion.identity;
-        idleAnimName = getRandomIdleAnim(1, 5);
+        distToGround = thePlayer.GetComponent<BoxCollider>().bounds.extents.y;
+
+        characterController = thePlayer.GetComponent<CharacterController>();
+
     }
 
     void Update()
     {
+        //AnimationControl.PLAYER_STATE = AnimationControl.IDLE;
+
+
+        //[0.5.0] Disable character control if UI is active
         
 
-        if (Input.GetButton("Horizontal") ||
-            Input.GetButton("Vertical"))
+        if (characterController.isGrounded)
         {
-            //[0.5.0] Disable character control if UI is active
-            if (ButtonHandler.victoryActive || ButtonHandler.startActive)
-                return;
+            AnimationControl.PLAYER_STATE = AnimationControl.IDLE;
+            if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+            {
 
-            if (CarryItem.isCarrying)
-            {
-                thePlayer.GetComponent<Animator>().Play("RunWIthObject");
-                fastRunning = false;
-            }
-            else if (Input.GetButton("Shift"))
-            {
-                thePlayer.GetComponent<Animator>().Play("Fast Run");
-                fastRunning = true;
+                if (ButtonHandler.victoryActive || ButtonHandler.startActive)
+                    return;
+
+                isRunning = true;
+
+                //Animation State Control
+                if (CarryItem.isCarrying)
+                {
+                    AnimationControl.PLAYER_STATE = AnimationControl.RUNNING_CARRYING;
+                    fastRunning = false;
+                }
+                else if (Input.GetButton("Shift"))
+                {
+                    AnimationControl.PLAYER_STATE = AnimationControl.RUNNING_FAST;
+                    fastRunning = true;
+                }
+                else
+                {
+                    AnimationControl.PLAYER_STATE = AnimationControl.RUNNING;
+                    fastRunning = false;
+                }
+                //~~~ Animation State Control
+
+                //[0.6.0] Character rotation now performed with mouse
+                //horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
+
+
+                ZMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;
+                XMove = Input.GetAxis("Horizontal") * Time.deltaTime * 8;
             }
             else
             {
-                thePlayer.GetComponent<Animator>().Play("RunNinja");
+                //No movement with W,A,S,D
                 fastRunning = false;
+                if (isRunning)
+                {
+                    isRunning = false;
+                    //normalIdleTimeStamp = stopwatch.ElapsedMilliseconds;
+
+                    AnimationControl.PLAYER_STATE = AnimationControl.IDLE;
+
+                    if (CarryItem.isCarrying)
+                    {
+                        AnimationControl.PLAYER_STATE = AnimationControl.IDLE_CARRYING;
+                    }
+                }
+
+                if (!isRunning && CarryItem.isCarrying)
+                {
+                    AnimationControl.PLAYER_STATE = AnimationControl.IDLE_CARRYING;
+                }
+
+                ZMove = XMove = 0;
+
+                //[0.6.0] Sleeping State is temporarily removed 
+                /*
+                else if (stopwatch.ElapsedMilliseconds - normalIdleTimeStamp > 5000)
+                    idleAnimName = "Sleeping Idle";
+                */
             }
-
-
-            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
-            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;
 
             if (fastRunning)
-                verticalMove *= 1.5f;
-
-            isRunning = true;
-            if (!isStepping)
             {
-                StartCoroutine(RunSound());
+                ZMove *= 1.5f;
+                XMove *= 1.5f;
             }
 
-            transform.Rotate(0, horizontalMove, 0);
-            transform.Translate(0, 0, verticalMove);
+            if (Input.GetButton("Jump") && !CarryItem.isCarrying)
+            {
+                //Jumping
+                YMove = jumpSpeed;
+                isJumping = true;
+                if (isRunning)
+                    AnimationControl.PLAYER_STATE = AnimationControl.JUMPING_RUNNING;
+                else
+                    AnimationControl.PLAYER_STATE = AnimationControl.JUMPING_INPLACE;
+            }
 
-            //Debug.Log("Player angle is " + thePlayer.transform.eulerAngles.x +
-            //    " " + thePlayer.transform.eulerAngles.y + " " + thePlayer.transform.eulerAngles.z);
-            //UnityEngine.Debug.Log("Player Y Angle is " + thePlayer.transform.eulerAngles.y);
-        }
-        else
+        } //~~~ if isGrounded
+
+        //[0.6.0] Character rotation now performed with mouse
+        //Camera rotation with mouse
+        if (!ButtonHandler.victoryActive && !ButtonHandler.startActive)
         {
-            fastRunning = false;
-            if (isRunning)
-            {
-                isRunning = false;
-                normalIdleTimeStamp = stopwatch.ElapsedMilliseconds;
-                idleAnimName = getRandomIdleAnim(1, 5);
-
-                if (CarryItem.isCarrying)
-                    idleAnimName = "CarryingIdle";
-            }
-
-            if (!isRunning && CarryItem.isCarrying)
-            {
-                idleAnimName = "CarryingIdle";
-            }
-            else if (changeToNormalAnimation)
-            {
-                normalIdleTimeStamp = stopwatch.ElapsedMilliseconds;
-                changeToNormalAnimation = false;
-                idleAnimName = getRandomIdleAnim(1, 5);
-            }
-            else
-            {
-                if (stopwatch.ElapsedMilliseconds - normalIdleTimeStamp > 5000)
-                {
-                    idleAnimName = "Sleeping Idle";
-                }
-            }
-          
-
-            thePlayer.GetComponent<Animator>().Play(idleAnimName);
-
+            float mouseX = Input.GetAxis("Mouse X");
+            if (mouseX < 0) mouseX = -1;
+            else if (mouseX > 0) mouseX = 1;
+            transform.Rotate(0, mouseX * camRotateSpeed * Time.deltaTime, 0);
         }
+
+        //isRunning = true;
+        if (!isStepping && (ZMove != 0 || XMove != 0) && !isJumping)
+            StartCoroutine(RunSound());
+
+        YMove -= gravity * Time.deltaTime;
+        /*if (IsGrounded())
+            YMove = 0.0f;*/
+
+        //[0.6.0] Character rotation now performed with mouse
+        //transform.Rotate(0, horizontalMove, 0);
+
+        //[0.6.0] Attempt to use CharacterController
+        Vector3 moveDirection = new Vector3(XMove, YMove, ZMove);
+        moveDirection = moveDirection * speed;
+        moveDirection.y = YMove;
+        moveDirection = 
+            Quaternion.Euler(
+                transform.eulerAngles.x, 
+                transform.eulerAngles.y, 
+            transform.eulerAngles.z) *
+            moveDirection;
+        characterController.Move(moveDirection * Time.deltaTime);
+        //transform.Translate(XMove, YMove, ZMove); //--
     }
 
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(thePlayer.transform.position, -Vector3.up, distToGround + 0.1f);
+    }
 
 
     IEnumerator RunSound()
@@ -139,29 +202,11 @@ public class CharControl : MonoBehaviour
             }
         }
         if (fastRunning)
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.21f);
         else
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.315f);
         isStepping = false;
     }
 
-    string getRandomIdleAnim(int min, int max)
-    {
-        int val = Random.Range(min, max);
 
-        string str;
-
-        switch (val)
-        {
-            case 0: str = "Victory Idle"; break;
-            case 1: str = "Kneeling Idle"; break;
-            case 2: str = "Goalkeeper Idle"; break;
-            case 3: str = "Ninja Idle"; break;
-            case 4: str = "Happy Idle"; break;
-            case 5: str = "Sleeping Idle"; break;
-            default: str = "Ninja Idle"; break;
-        }
-
-        return str;
-    }
 }
